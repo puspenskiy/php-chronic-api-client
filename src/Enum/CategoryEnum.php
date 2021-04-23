4a7a63342690f31deb836e6d72823336930ef84e
Гревцов Александр <agrevtsov@sberhealth.ru>
2021-04-23 14:59:33 +0400

    Фикс русской буквы в названии категории

--- src/Enum/CategoryEnum.php
+++ src/Enum/CategoryEnum.php
@@ -10,7 +10,7 @@ class CategoryEnum extends AbstractBaseEnum
 	public const COVID = 'covid';
 	public const HYPERTONIC = 'hypertonic';
 	public const DIABETIC = 'diabetic';
-	public const CORONARY_ARTERY = 'сoronary_artery';
+	public const CORONARY_ARTERY = 'coronary_artery';
 	public const HEART_FAILURE = 'heart_failure';
 	public const WARFARIN = 'warfarin';
 }
